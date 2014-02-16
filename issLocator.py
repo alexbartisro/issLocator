7e27b422b8242c532deb9680cb6b6e3c3633dee6
trusk89 <alex.bartis@gmail.com>
2014-02-16 14:24:51 +0200

    Dropped geopy geolocator Added pygeocoder librabry for reverse geocoding
    know issues:
    - program crashes if address is unknown

--- issLocator.py
+++ issLocator.py
@@ -1,45 +1,41 @@
 
 import urllib, json, threading
-from geopy import geocoders
+from pygeocoder import Geocoder
 
-geolocator =  geocoders.GoogleV3()
 url= "https://api.wheretheiss.at/v1/satellites/25544";
 latitude = 0;
 longitude = 0;
 address = "";
 lastKnownAddress = "";
-point = ""; 
 
 def work():
 	global latitude; 
 	global longitude; 
 	global address; 
 	global lastKnownAddress; 
-	global point;
         response = urllib.urlopen(url);
         data = json.loads(response.read());
-        name = data['name'];
         latitude = data['latitude'];
-        longitude = data['longitude'];
-       	strLong = '%.7f' % longitude;
-	strLat = '%.7f' % latitude;
-	point = strLat + ", " + strLong; 
-	threading.Timer(1, work).start();
-       	address = geolocator.reverse ("latitude", "longitude");
+        longitude = data['longitude']; 
+	try:       
+		address = Geocoder.reverse_geocode(latitude, longitude);
+	except ValueError:
+		print "No known address";
 	if address != None:
 		lastKnownAddress = address;
 	printCoordinates();	
+	threading.Timer(1, work).start();
+
 
 def printCoordinates():
-	print "Point is ", point;
 	print " ";
+	print "Last know address is ", lastKnownAddress;
         print "The International Space Station's current coordinates are ";
         print "Latitude =",latitude," ","Longitude =",longitude;
-	if ( lastKnownAddress != None and lastKnownAddress !="" ):
+#	if ( lastKnownAddress != None and lastKnownAddress !="" ):
 	#	print "Current address is ";
 	#	print address;
-		print "Last known Address is ";
-		print lastKnownAddress;
-		
+#		print "Last known Address is ";
+#		print lastKnownAddress;
 
 work();
